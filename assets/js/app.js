2962acc09880a1390ec37a85f2963acfdad82bdb
Yusuf AbdulMuizz <yusufabdulmuizz00@gmail.com>
2020-09-18 01:12:49 +0100

    first comiit

---
+++ assets/js/app.js
@@ -0,0 +1,66 @@
+// to input only numbers in phoneinput
+// function a(event){
+//  var Number = event.which;
+//  if (Number >= 48  && Number <=57 ) {
+//      return false;
+//  }
+// }
+
+
+function regFormValidate(){
+var email=document.getElementById('regEmail').value;
+var password=document.getElementById('regPassword').value;
+var confirmPassword=document.getElementById('regConfirm').value;
+var firstName=document.getElementById('regFirstname').value;
+var lastName=document.getElementById('regSurname').value;
+var phone=document.getElementById('regPhone').value;
+var address=document.getElementById('regAddress').value;
+var answer=document.getElementById('regAnswer');
+var radio =document.getElementsByClassName('gender');
+
+
+if (email =="" || email==null ) {
+    answer.innerHTML="please fill the email";
+    email.focus;
+    return false;
+}
+if (password == ""   ) {
+    answer.innerHTML="Fill Password";
+    password.focus;
+    return false;
+}
+if(password.length <=6){
+    answer.innerHTML="password too short";
+    password.focus;
+    return false;
+}
+if (confirmPassword !== password || confirmPassword=="" || confirmPassword==null ) {
+    answer.innerHTML="Comfirm Password";
+    return false;
+}
+if (firstName == "" ) {
+    answer.innerHTML="fill Firstname";
+    firstName.focus;
+    return false;
+}
+if (lastName == "" ) {
+    answer.innerHTML="fill LastName";
+    lastName.focus;
+    return false;
+}
+if (phone == "") {
+    answer.innerHTML="fill Phone";
+    phone.focus;
+    return false;
+}
+if (address == "" ) {
+    answer.innerHTML="fill address";
+    address.focus;
+    return false;
+}
+if ((radio[1].checked == false) && (radio[2].checked == false) && (radio[2].checked == false)) {
+    answer.innerHTML=" select a gender";
+    return false;
+} 
+return true;
+}
